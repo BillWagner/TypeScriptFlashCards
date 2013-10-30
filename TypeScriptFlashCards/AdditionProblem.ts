25cbbd58c1b8c5db34867a41cf132829d592d648
Bill Wagner <bill.w.wagner@outlook.com>
2013-10-30 14:43:45 -0700

    Implement the constructor
    Now the basic application is done

--- TypeScriptFlashCards/AdditionProblem.ts
+++ TypeScriptFlashCards/AdditionProblem.ts
@@ -3,6 +3,31 @@ module MathProblems {
         element: HTMLElement;
 
         constructor(element: HTMLElement) {
+            var left = Math.floor(Math.random() * 25);
+            var right = Math.floor(Math.random() * 25);
+            var correctAnswer = left + right;
+
+            var paragraph = document.createElement('p');
+            paragraph.innerHTML = left.toString() + " + " + right.toString() + " = ";
+
+            var txt = document.createElement('input');
+            txt.type = 'text';
+            paragraph.appendChild(txt);
+
+            var button = document.createElement('input');
+            var msg = document.createElement('span');
+            button.type = 'button';
+            button.value = "Check Answer";
+            button.onclick = function () {
+                if (parseInt(txt.value) == correctAnswer)
+                    msg.innerHTML = "You are correct";
+                else
+                    msg.innerHTML = "Please try again";
+            };
+            paragraph.appendChild(button);
+            paragraph.appendChild(msg);
+
+            element.appendChild(paragraph);
         }
 
     }
