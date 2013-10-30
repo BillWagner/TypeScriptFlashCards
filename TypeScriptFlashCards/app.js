9be4a1dcfe76e484724ebe68f9c166303433a149
Bill Wagner <bill.w.wagner@outlook.com>
2013-10-30 14:34:19 -0700

    Remove boilerplate implementation of the greeter class
    This is now an empty class that will be modified to contain the math
    problem

--- TypeScriptFlashCards/app.js
+++ TypeScriptFlashCards/app.js
@@ -1,21 +1,6 @@
 var AdditionProblem = (function () {
     function AdditionProblem(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
     }
-    AdditionProblem.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () {
-            return _this.span.innerHTML = new Date().toUTCString();
-        }, 500);
-    };
-
-    AdditionProblem.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
     return AdditionProblem;
 })();
 
